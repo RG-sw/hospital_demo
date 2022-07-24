fccd813ec5cc83d5f434386e6b9cfed3ef2b81a5
Roberto Gaio <gaio.roberto.gr@gmail.com>
2022-07-24 17:36:11 +0200

    added first auth mechanism

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from typing import List, Union
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, WebSocket
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import crud, models, schemas
 from database import SessionLocal, engine
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,24 +19,65 @@ def get_db():
     finally:
         db.close()
 
+@app.websocket("/ws")
+async def get_data(websocket: WebSocket):
+    await websocket.accept()
+    input_text = await websocket.receive_text()
+    print(input_text)    
+    
 
-@app.get("/sometable/", response_model=schemas.SomeTable)
-def read_users(id: int, db: Session = Depends(get_db)):
+@app.get("/sometable/{id}", response_model=schemas.SomeTable)
+def read_user(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     users = crud.get_item(db, id)
     print("inside main", users)
     return users
 
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: schemas.SomeTable):
     return {"item": item, "item_id": item_id}
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    # user = UserInDB(**user_dict)
+    hashed_password = form_data.password  #fake_hash_password(form_data.password)
+    if not hashed_password == user_dict['hashed_password']:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user_dict['username'], "token_type": "bearer"}
+
+
+fake_users_db = {
+    "user": {
+        "username": "user",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "web_dev",
+        "disabled": False,
+    },
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
--- frontend/app.js
+++ frontend/app.js
@@ -5,6 +5,13 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+let ws = new WebSocket("ws://127.0.0.1:8000/ws");
+// ws.onopen = function(e) {
+//     alert("[open] Connection established");
+//     alert("Sending to server");
+//     ws.send("My name is John");
+//   };
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
@@ -12,8 +19,14 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
+        // ws = new WebSocket("ws://127.0.0.1:8000/ws");
+        // ws.onopen = function(e) {
+        //     ws.send("My name is John");
+        // }
+        ws.send(username);
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
