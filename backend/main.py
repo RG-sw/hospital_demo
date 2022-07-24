a7a04be9f75c89cd309aa339c0c00d98cbdbe3f0
Roberto Gaio <gaio.roberto.gr@gmail.com>
2022-07-24 20:14:30 +0200

    tested api authentication

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,24 @@ from fastapi import Depends, FastAPI, HTTPException, WebSocket
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
+from jose import JWTError, jwt
+import bcrypt
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+
 import crud, models, schemas
 from database import SessionLocal, engine
+
+from fastapi.middleware.cors import CORSMiddleware  # NEW
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = 'f29960e83d12aa467c08329a283d64463e95f33a61d0eed11a13607062df5d69'
+ALGORITHM = "HS256"
 
 def get_db():
     db = SessionLocal()
@@ -47,23 +58,53 @@ def update_item(item_id: int, item: schemas.SomeTable):
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    # user = UserInDB(**user_dict)
-    hashed_password = form_data.password  #fake_hash_password(form_data.password)
-    if not hashed_password == user_dict['hashed_password']:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user['username']}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-    return {"access_token": user_dict['username'], "token_type": "bearer"}
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user['hashed_password']):
+        return False
+    return user
 
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return user_dict
+
+def verify_password(plain_password, hashed_password):
+    ret = bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password)
+    return ret
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})  # adds an expire field to the dict
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 fake_users_db = {
     "user": {
         "username": "user",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "web_dev",
+        "hashed_password": b'$2b$12$8XgFKvTt1KyoXNsrPV9fy.OIDTUt4wRbLI.Gs0Y.bpXBWr0KFBVna',
         "disabled": False,
     },
     "johndoe": {
